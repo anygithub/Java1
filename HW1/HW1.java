a187bf3d5ea6dadf8175e24a3361fdbf1d0793bb
Anna Bondarenko <postportal@mail.ru>
2019-01-13 13:34:58 +0300

    initiateVariables method added

--- HW1/HW1.java
+++ HW1/HW1.java
@@ -1,5 +1,18 @@
 public class HW1{
 	public static void main (String[] args){
-		System.out.println("Hello World!");
+		initiateVariables();
+	}
+	
+	static void initiateVariables(){
+		byte bt = 120;
+		short st = 32700;
+		int it = 45000;
+		long lng = 900000L;
+		float fl = 5689.7f;
+		double dbl = 468.45489;
+		boolean bl = true;
+		char ch = 'r';
+		
+		String str = "First homework";
 	}
 }
