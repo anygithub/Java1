7985ad112c195df446cdece1d1def81209280b43
Anna Bondarenko <postportal@mail.ru>
2019-01-15 19:30:51 +0300

    javadoc comments were added replaceZerosAndOnes() method was added

--- HW2/HW2.java
+++ HW2/HW2.java
@@ -1,3 +1,19 @@
+/**
+ * Java1. Homework2
+ *
+ * @author Anna Bondarenko
+ * @version date 
+*/
+
 class HW2 {
+
+    public static void main(String[] args) {
+        
+        
+    }
     
+    public static void replaceZerosAndOnes() {
+        
+    }
+
 }
