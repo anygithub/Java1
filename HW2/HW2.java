c2893771bdc290a01246ccb6c920746370ee1ecf
Anna Bondarenko <postportal@mail.ru>
2019-01-15 21:38:26 +0300

    code was added in replaceZerosAndOnes method

--- HW2/HW2.java
+++ HW2/HW2.java
@@ -4,16 +4,27 @@
  * @author Anna Bondarenko
  * @version date 
 */
+import java.util.Arrays;
 
 class HW2 {
 
     public static void main(String[] args) {
-        
+        //call 
+        replaceZerosAndOnes();
         
     }
     
+    // Method wich replace 0 by 1 and 1 by 0 in array
     public static void replaceZerosAndOnes() {
-        
+        int[] arr = {1,1,0,0,1,0,1,1,0,0};
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 1) { 
+                arr[i] = 0;
+            } else {
+                arr[i] = 1;
+            }
+        }
+        System.out.println(Arrays.toString(arr));
     }
 
 }
