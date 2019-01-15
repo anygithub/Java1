77055ea9febed6109ee9af03dafcc1e9d1f58936
Anna Bondarenko <postportal@mail.ru>
2019-01-15 21:45:50 +0300

    createArray method was added

--- HW2/HW2.java
+++ HW2/HW2.java
@@ -9,11 +9,13 @@ import java.util.Arrays;
 class HW2 {
 
     public static void main(String[] args) {
-        //call 
+        //call task1
         replaceZerosAndOnes();
-        
+
+        //call task2
+        createArray();
     }
-    
+
     // Method wich replace 0 by 1 and 1 by 0 in array
     public static void replaceZerosAndOnes() {
         int[] arr = {1,1,0,0,1,0,1,1,0,0};
@@ -25,6 +27,15 @@ class HW2 {
             }
         }
         System.out.println(Arrays.toString(arr));
+    }
+
+    //Create array with length 8 and fill it
+    public static void createArray() {
+        int [] ar = new int [8];
+        for (int i = 0; i < 8; i++) {
+            ar [i] = i*3;
+        }
+        System.out.println(Arrays.toString(ar));
     }
 
 }
