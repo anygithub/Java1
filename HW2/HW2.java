bdf7ed67aba243b1634dde1e9d57b5c51b212c32
Anna Bondarenko <postportal@mail.ru>
2019-01-16 09:56:28 +0300

    start to add javadoc comment

--- HW2/HW2.java
+++ HW2/HW2.java
@@ -25,7 +25,9 @@ class HW2 {
        
     }
 
-    // Method wich replace 0 by 1 and 1 by 0 in array
+    /**
+     * Method wich replace 0 by 1 and 1 by 0 in array
+     */
     public static void replaceZerosAndOnes() {
         int[] arr = {1,1,0,0,1,0,1,1,0,0};
         for (int i = 0; i < arr.length; i++) {
@@ -38,7 +40,9 @@ class HW2 {
         System.out.println(Arrays.toString(arr));
     }
 
-    //Create array with length 8 and fill it
+    /**
+     * Create array with length 8 and fill it
+     */
     public static void createArray() {
         int[] ar = new int [8];
         for (int i = 0; i < 8; i++) {
@@ -47,7 +51,9 @@ class HW2 {
         System.out.println(Arrays.toString(ar));
     }
 
-    //Create array and multiple each element which is lower then 6 on 2
+    /**
+     * Create array and multiple each element which is lower then 6 on 2
+     */
     public static void createArrayAndMultipleElements() {
         int[] arr = {1,5,3,2,11,4,5,2,4,8,9,1};
         for (int i = 0; i < arr.length; i++) {
@@ -58,7 +64,9 @@ class HW2 {
         System.out.println(Arrays.toString(arr));
     }
 
-    //Creates two-dimensional array and fills diagonal elements with 1
+    /**
+     * Creates two-dimensional array and fills diagonal elements with 1
+     */
     public static void createTwodimensionalArray(int n) {
         int[][] matr = new int [n][n];
         for (int i = 0; i < n; i++) {
@@ -72,7 +80,9 @@ class HW2 {
         }
     }
 
-    //Finds maximum and minimun eleemt in the array
+    /**
+     * Finds maximum and minimun eleemt in the array
+     */
     public static void findMaxAndMinElementsInArray() {
         
     }
