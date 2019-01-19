60ed9ad663eea2121b1d95ff0b41e896f1241c69
Anna Bondarenko <postportal@mail.ru>
2019-01-19 12:42:34 +0300

    inputNumber method added

--- HW3/HW3.java
+++ HW3/HW3.java
@@ -5,24 +5,21 @@
  * @version date 
 */
 
+import java.util.Scanner;
+
 class HW3 {
     public static void main (String[] args) {
-        
+        inputNumber(1,9);
+        //compareNumbers()
     }
 
     /**
-     * Compare two numbers.
-     * @param num1 - integer number
-     * @param num2 - integer number
+     * Method requests to input random number from the range.
      */
-    static String compareNumbers(int num1, int num2) {
-        if (num1 > num2) {
-            return "Your number is greater then specified number" ;
-        } else if (num1 < num2) {
-            return "Your number is less then specified number" ;
-        } else {
-            return "Congratulations! You guessed the number."
-        }
+    public static int inputNumber(int min, int max) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input number in a range from " + min + " to " + max + ": ");
+        return sc.nextInt();
     }
 
     /**
@@ -30,7 +27,25 @@ class HW3 {
      * @param min - lower number of the range
      * @param max - higher number of the range
      */
-    static int generateRandomNumber(int min, int max) {
-        return Math.random
+    /*static int generateRandomNumber(int min, int max) {
+        //Random rand = new Random();
+        return 0;//(min + rand.nextInt(max + 1));
     }
+
+    /**
+     * Compare two numbers.
+     * @param num1 - integer number
+     * @param num2 - integer number
+     */
+    /*static String compareNumbers(int num1, int num2) {
+        if (num1 > num2) {
+            return ("Your number is greater then specified number");
+        } else if (num1 < num2) {
+            return ("Your number is less then specified number");
+        } else {
+            return ("Congratulations! You guessed the number.");
+        }
+    } */
+
+
 }
