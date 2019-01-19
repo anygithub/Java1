08d85dec7f01d281c46efb6816d8e35e7dbcd73d
Anna Bondarenko <postportal@mail.ru>
2019-01-19 11:45:27 +0300

    compare numbers method is added

--- HW3/HW3.java
+++ HW3/HW3.java
@@ -9,4 +9,22 @@ class HW3 {
     public static void main (String[] args) {
         
     }
+
+    /**
+     * Check if two numbers are equal. If they are not equal return ????
+     * @param num1 - integer number
+     * @param num2 - integer number
+     */
+    static String checkNumber(int num1, int num2) {
+        
+    }
+
+    /**
+     * Method generates random number.
+     * @param min - lower number of the range
+     * @param max - higher number of the range
+     */
+    static int generateRandomNumber(int min, int max) {
+        return Math.random
+    }
 }
