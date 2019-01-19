be2db95f80d7e404211b8740e75835f3a970af72
Anna Bondarenko <postportal@mail.ru>
2019-01-19 13:33:36 +0300

    runGame and playAgain methods added

--- HW3/HW3.java
+++ HW3/HW3.java
@@ -10,13 +10,14 @@ import java.util.Random;
 
 class HW3 {
     public static void main (String[] args) {
-        inputNumber(1,9);
-        System.out.println(generateRandomNumber(1,9));
-        //compareNumbers()
+        
+        //
     }
 
     /**
      * Method requests to input random number from the range.
+     * @param min - lower number of the range
+     * @param max - higher number of the range
      */
     static int inputNumber(int min, int max) {
         Scanner sc = new Scanner(System.in);
@@ -39,7 +40,7 @@ class HW3 {
      * @param num1 - integer number
      * @param num2 - integer number
      */
-    /*static String compareNumbers(int num1, int num2) {
+    static String compareNumbers(int num1, int num2) {
         if (num1 > num2) {
             return ("Your number is greater then specified number");
         } else if (num1 < num2) {
@@ -47,7 +48,30 @@ class HW3 {
         } else {
             return ("Congratulations! You guessed the number.");
         }
-    } */
+    } 
 
+    /**
+     * Runs tha game where computer generates random number and user have 3 attempts to gues it.
+     * @param option - '1' continue the game; '0' stop the game;
+     */
+    static playAgain(byte option) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Play again? (1 - Yes; 0 - No)");
+        option = sc.nextByte();
+    }
+
+    /**
+     * Runs tha game where computer generates random number and user have 3 attempts to gues it.
+     * @param option - '1' continue the game; '0' stop the game;
+     */
+    static void runGame(byte option) {
+        byte i = 1;
+        
+        while(option == 1) {
+            compareNumbers(inputNumber(1,9),generateRandomNumber(1,9));
+            i++;
+            playAgain(); //&???
+        }
+    }
 
 }
