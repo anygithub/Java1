72aad9ddb594355b5ab2f3610cbea7258827dbff
Anna Bondarenko <postportal@mail.ru>
2019-01-25 21:40:34 +0300

    Employee class created

---
+++ HW5/Employee.java
@@ -0,0 +1,18 @@
+/**
+ * Java1. Homework5
+ *
+ * @author Anna Bondarenko
+ * @version date 
+*/
+
+import java.util.Scanner;
+import java.util.Random;
+import java.util.Arrays;
+
+class Employee {
+    public static void main (String[] args) {
+        
+    }
+
+    
+}
