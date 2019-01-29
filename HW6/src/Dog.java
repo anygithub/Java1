fa72805512749f287e15e98fcddc1ca6100fd698
Anna Bondarenko <postportal@mail.ru>
2019-01-29 17:32:27 +0300

    Dog class added

---
+++ HW6/src/Dog.java
@@ -0,0 +1,13 @@
+/**
+ * Java1. Homework6
+ *
+ * @author Anna Bondarenko
+ * @version date
+ */
+
+public class Dog extends  Animal{
+
+    public Dog(String name, double runDistance, double swimDistance, double jumpHight) {
+        super(name, runDistance, swimDistance, jumpHight);
+    }
+}
